6aadc913fcd5fabe5a279386bbae8a4aa2f03e52
Onkar774 <sandhuonkar53@gmail.com>
2026-03-19 10:00:45 -0500

    Optimize script for background usage

--- script.sh
+++ script.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-python finance.py >> data.txt
+
+do_daemon_work() {
+    while true; do
+        python finance.py >> data.txt
+        sleep 3600
+    done
+}
+
+do_daemon_work & disown
